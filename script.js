6e742a8acd6bb7120542020dd0257be5ff74c8af
gltats <74324703+gltats@users.noreply.github.com>
2022-01-03 22:07:48 +0100

    how to do grids

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ context.beginPath();
 context.strokeStyle = "red";
 context.arc(210, 150, 50, 0, 2 * Math.PI);
 context.stroke();
+
+
+for (let i = 0; i < 5; i++){
+for (let j = 0; j < 3; j++){
+let width = 30;
+let height = 30;
+let gap = 30;
+let x = 75 + (width + gap)* i;
+let y = 75 + (width + gap)* j;   
+
+context.beginPath();
+context.rect(x, y, width, height);
+context.strokeStyle = "green";
+context.stroke();
+
+}
+}
